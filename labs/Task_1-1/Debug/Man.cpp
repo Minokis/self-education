03f7d9d62ba2028de8e6905bb2f67ec2a73d3859
Anna Korniychuk <korniychuk.anna@gmail.com>
2017-08-22 19:20:18 +0300

    Debugging is completed

--- labs/Task_1-1/Debug/Man.cpp
+++ labs/Task_1-1/Debug/Man.cpp
@@ -1,12 +1,10 @@
 // реализация класса Man
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
 #include "Man.h"
 
-const int l_name = 30;
-const int l_year = 5;
-const int l_pay = 10;
-const int l_buf = l_name + l_year + l_pay;
+
 
 
 
@@ -20,16 +18,24 @@ Man::~Man() {
 }
 
 void Man::SetName(const char* fromBuf) {
-	if (pName == NULL) {
-		std::cout << "problem\n";
-	}
-	strncpy_s(pName, l_name, fromBuf, _TRUNCATE);
+	strncpy_s(pName, l_name - 1, fromBuf, _TRUNCATE);
 	pName[l_name] = '\0';
 }
-//void Man::SetBirthYear(const char* fromBuf) {
-//	
-//}
-//void Man::SetPay(const char* fromBuf) {
-//	
-//}
+void Man::SetBirthYear(const char* fromBuf) {
+	birth_year = atoi(fromBuf + l_name);
+}
+void Man::SetPay(const char* fromBuf) {
+	pay = atof(fromBuf + l_name + l_year);
+}
 
+void Man::Print() {
+	std::cout << pName << "\t" << birth_year << "\t" << std::setprecision(10) << pay << std::endl;
+}
+
+bool Man::CompareName(const char* name) {
+	if (strstr(pName, name) && pName[strnlen_s(name, l_name)] == ' ')
+		return true;
+	else
+		return false;
+		
+}
--- labs/Task_1-1/Debug/Man.h
+++ labs/Task_1-1/Debug/Man.h
@@ -2,6 +2,11 @@
 #ifndef MAN_H
 #define MAN_H
 
+const int l_name = 30;
+const int l_year = 5;
+const int l_pay = 10;
+const int l_buf = l_name + l_year + l_pay;
+
 class Man {
 public:
 	Man(int lName = 30);
@@ -11,9 +16,12 @@ public:
 	int GetBirthYear() const { return birth_year; }
 	float GetPay() const { return pay; }
 
-	void SetName(const char* fromBuf);
-	//void SetBirthYear(const char*);
-	//void SetPay(const char*);
+	void SetName(const char*);
+	void SetBirthYear(const char*);
+	void SetPay(const char*);
+
+	void Print();
+	bool CompareName(const char*);
 private:
 	char* pName;
 	int birth_year;
