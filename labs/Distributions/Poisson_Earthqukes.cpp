b2f6d1a137f5ae316f350e882ef733debee512bd
Anna Korniychuk <anna.korniychuk@itiviti.com>
2018-05-09 13:41:33 +0300

    Simple generator

--- labs/Distributions/Poisson_Earthqukes.cpp
+++ labs/Distributions/Poisson_Earthqukes.cpp
@@ -12,3 +12,34 @@ http://preshing.com/20111007/how-to-generate-random-timings-for-a-poisson-proces
 
 
 */
+
+
+#include <cmath>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+float nextTime(float rateParameter)
+{
+    return -(-1.0f/rateParameter) * log(1-static_cast<float>(rand())/(RAND_MAX+1));
+}
+
+double check_poisson(float rateParameter, int iterations) {
+  double sum = 0;
+  for(int i=0; i<iterations;++i) {
+    sum += static_cast<double>(nextTime(rateParameter));
+  }
+  return sum/iterations;
+}
+
+int main() {
+  //using namespace std;
+  printf("RAND_MAX is %d\n", RAND_MAX);
+  srand(time(NULL));
+  float lam = 1.0f/40;
+  for(int i=0; i<5;i++)
+    printf("%lf\t", nextTime(lam));
+    //TODO: find a bug. Check shows that calculations are wrong. Or check function is written wrong.
+  printf("Check: %lf\n", check_poisson(lam, 1000) );
+  return 0;
+}
