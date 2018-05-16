8978e15c86bf2f3dc4da3ebfb7549d404db5b99a
Anna Korniychuk <korniychuk.anna@gmail.com>
2018-05-17 00:42:14 +0300

    finish first loop

--- labs/part_1_Structured/2_ifs_and_loops/2_ifs_and_loops/2_4_values_table/2_4_values_table.cpp
+++ labs/part_1_Structured/2_ifs_and_loops/2_ifs_and_loops/2_4_values_table/2_4_values_table.cpp
@@ -11,28 +11,28 @@
 	y = tx, 0 <= x < 10
 		2t, x >= 10
 
-		���� t>100, ���������� ����� ��������.
+		If t>100, output is in integers.
 */
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	double Xn, Xk, dX, t, y;
-	printf("������� ���������, �������� �������� ���������, ��� � �������� �������: ");
+	printf("Enter Xn, Xk, dX, t:");
 	scanf_s("%lf%lf%lf%lf", &Xn, &Xk, &dX, &t);
-	printf("����������� �����: %lf %lf %lf %lf\n", Xn, Xk, dX, t);
-	printf("-------------------------\n");
-	printf("|    X    |    Y    |\n");
-	printf("-------------------------\n");
-	for (double x = Xn; x < Xk; x +=dX) {
+	printf("Control output: %lf %lf %lf %lf\n", Xn, Xk, dX, t);
+	printf("---------------------\n");
+	printf("|       X    |    Y    |\n");
+	printf("---------------------\n");
+	for (double x = Xn; x <= Xk; x +=dX) {
 		if (x < 0)            y = t;
 		if (x >= 0 && x < 10) y = t * x;
 		if (x >= 10)          y = 2 * t;
 		
-		if (t>100) printf("|    %lf    |    %lf    |\n", x, y);
-		else printf("|    %lf    |    %d    |\n", x, y);
+		if (t > 100) printf("|%9.2lf    |%9d    |\n", x, static_cast<int>(y));
+		else printf("|%9.2lf    |%9.2lf    |\n", x, y);
 
 	}
-    printf("-------------------------\n");
+    printf("---------------------\n");
     return 0;
 }
