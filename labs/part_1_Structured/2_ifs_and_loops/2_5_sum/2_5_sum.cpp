64845d7623482dd29d37779eca974f09bbfe1be3
Anna <korniychuk.anna@gmail.com>
2018-05-22 12:26:47 +0300

    Update 2_5_sum.cpp

--- labs/part_1_Structured/2_ifs_and_loops/2_5_sum/2_5_sum.cpp
+++ labs/part_1_Structured/2_ifs_and_loops/2_5_sum/2_5_sum.cpp
@@ -1,5 +1,5 @@
-// 2_5_sum.cpp: определяет точку входа для консольного приложения.
-// Задача на сходимость суммы ряда
+// 2_5_sum.cpp: Г®ГЇГ°ГҐГ¤ГҐГ«ГїГҐГІ ГІГ®Г·ГЄГі ГўГµГ®Г¤Г  Г¤Г«Гї ГЄГ®Г­Г±Г®Г«ГјГ­Г®ГЈГ® ГЇГ°ГЁГ«Г®Г¦ГҐГ­ГЁГї.
+// Г‡Г Г¤Г Г·Г  Г­Г  Г±ГµГ®Г¤ГЁГ¬Г®Г±ГІГј Г±ГіГ¬Г¬Г» Г°ГїГ¤Г 
 
 #include "stdafx.h"
 #include <iostream>
@@ -10,16 +10,28 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	using namespace std;
-	cout << "Введите аргумент x и точность: ";
+	cout << "Г‚ГўГҐГ¤ГЁГІГҐ Г Г°ГЈГіГ¬ГҐГ­ГІ x ГЁ ГІГ®Г·Г­Г®Г±ГІГј: ";
 	double y = 1, current = 1, x, eps;
 	cin >> x >> eps;
 	const int MaxIter = 500;
 
-	for (int n = 1; n < 5; ++n) {
+	for (int n = 0; n < 5; ++n) {
 		current *= x * x / (2 * n + 1) / (2 * n + 2);
 		y += current;
-		cout << "current"
+		cout << "current = " << current << ", y = " << y << endl;
 	}
+	
+// 	for (int n = 0; n < MaxIter; ++n) {
+// 		current *= x * x / (2 * n + 1) / (2 * n + 2);
+// 		if(current < epsilon) {
+// 			cout << "Result: y = " << y << ", " << n << " iterations." << endl;
+// 			break;
+// 		}
+// 		y += current;
+// 		if (n = MaxIter-1) {
+// 			cout << "Couldn't reach the answer. Current result: y = " << y << ", " << n << " iterations." << endl;
+// 		}
+// 	}
     return 0;
 }
 
