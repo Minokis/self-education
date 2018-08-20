b22aaa57255b2482db66615d52ad0f0bb0617dae
Anna Korniychuk <anna.korniychuk@itiviti.com>
2018-08-20 10:27:18 +0300

    Add tree template

---
+++ baby_steps/tree_example.cpp
@@ -0,0 +1,20 @@
+// Let's create a binary tree of integers!
+#include <iostream>
+
+struct Node {
+  int d;
+  Node *left;
+  Node *right;
+};
+
+Node * first(int d);
+// Node * search_insert(Node *root, int d);
+// void print_tree(Node *root, int l);
+
+int main() {
+
+}
+
+Node * first(int d) {
+  
+}
