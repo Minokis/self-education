79d4b7a4b193bff1c0109da62adbbb5ac3932a09
Anna Korniychuk <anna.korniychuk@itiviti.com>
2018-08-24 11:16:28 +0300

    return to sorting task

--- labs/part_1_Structured/3_arrays/3_3_quicksort/3_3_quicksort.cpp
+++ labs/part_1_Structured/3_arrays/3_3_quicksort/3_3_quicksort.cpp
@@ -1,7 +1,7 @@
-// 3_3_quicksort.cpp: определяет точку входа для консольного приложения.
+// 3_3_quicksort.cpp: пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h" //uncomment in Visual Studio
 #include <iostream>
 #include <math.h>
 
@@ -16,19 +16,35 @@ int main()
 	int i, j, left, right;
 	/*cout << "Enter elements: ";
 	for (i = 0; i < n; i++) cin >> a[i];*/
-	float arr[n] = { 1.7, -18, 34.1, 1.8, 2.8, -9.2, -3, 0.6, 26.1, 100, 11, -2.5, -3, 0, 2.6, 7.1, -5, 5.9, 19, 0.7 };
 
-	// Поехали!
-//	sp = 1; stackl[1] = 0; stackr[1] = n - 1;
+	// Create an array
+	float arr[n] = { 1.7, -18, 34.1, 1.8, 2.8, -9.2, -3, 0.6, 26.1, 5, \
+		               11,  -2.5, -3,   0,  2.6,  7.1, -5, 5.9, 19,   0.7 };
 
-	left = 0; right = n - 1;
-	middle = arr[(left + right) / 2];
+  // Sorting
+	// step 1: sorted fragment is the array itself
+	sp = 1; stackl[1] = 0; stackr[1] = n - 1;
+	// while(sp > 0) {
+	  left = stackl[sp]; // 0
+		right = stackr[sp]; // 19
+		sp--;   // 0
+	  while (left < right) {
+			// Dividing arr[left] .. arr[right]
+			i = left; j = right;  // 0, 19
+			middle = arr[(left+right)/2];  // 5
+			while(i<j) {
+				while (arr[i] < middle) i++; // 1
+				while (middle < arr[j]) j--; // 19
+				if (i <= j) {
+					temp = arr[i]; arr[i] = arr[j]; arr[j] = temp; // { 1.7,  0.7  34.1, 1.8, 2.8, -9.2, -3, 0.6, 26.1, 5, \
+						                                             //    11, -2.5,   -3,   0, 2.6,  7.1, -5, 5.9, 19,  -18,  };
+					i++; j--;  //  2, 18
+				}
+			}
+		}
 
-	while()
 
-
-
+  // } // while(sp > 0)
 
     return 0;
 }
-
