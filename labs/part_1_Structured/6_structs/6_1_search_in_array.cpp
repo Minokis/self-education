c8ba72b7042b84ea4dcea63d6b4a42c43b1e297e
Anna Korniychuk <korniychuk.anna@gmail.com>
2018-10-11 13:38:47 +0300

    Finish flow version of 6_1

--- labs/part_1_Structured/6_structs/6_1_search_in_array.cpp
+++ labs/part_1_Structured/6_structs/6_1_search_in_array.cpp
@@ -25,14 +25,12 @@
 #include <fstream>
 #include <iostream>
 #include <limits>
+//#include <windows.h>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
   if(argc != 2) { cout << "Usage: enter the file with HR data.\n"; return 1; }
-  ifstream input(argv[1]);
-  if(!input) { cout << "Cannot open the file.\n"; return 1; }
-
   const int l_name = 30, l_year = 5, l_pay = 10, l_buf = l_name+l_year+l_pay;
 
   struct Man {
@@ -43,16 +41,56 @@ int main(int argc, char *argv[])
   const int l_dbase = 100;
   Man dbase[l_dbase];
 
+
   char name[l_name + 1];
-  char buf[l_buf+1];
+  char buf[l_buf + 1];
 
-  cout << "Enter a name: ";
-  cin.get(name, l_name+1);
-  cin.clear();
-  cin.ignore( numeric_limits<streamsize>::max(), '\n');
+  ifstream input(argv[1]);
+  if(!input) { cout << "Cannot open the file.\n"; return 1; }
 
 
+  // Reading from a file
+  int i = 0;
+  while( input.getline(buf, l_buf)) {
+    if (i >= l_dbase) { cout << "The file is too long. Stopping.\n"; return 1; }
+    strncpy(dbase[i].name, buf, l_name);
+    dbase[i].name[l_name] = '\0';
+    dbase[i].birth_year = atoi(&buf[l_name]);
+    dbase[i].pay = atof(&buf[l_name + l_year]);
+    i++;
+  }
+  int n_record = i, n_man = 0;
+  float mean_pay = 0;
 
+  while(true) {
+    cout << "Enter a name or a word \"end\": ";
+    // OemToChar(name, name);
+    cin.get(name, l_name+1);
+    cin.clear();
+    cin.ignore( numeric_limits<streamsize>::max(), '\n');
 
-  return 0;
+    if ( strcmp(name, "end") == 0 ) break;
+
+    bool not_found = true;
+    for( i = 0; i < n_record; i++) {
+      if ( strstr( dbase[i].name, name ) )
+        if ( dbase[i].name[strlen(name)] == ' ') { // check if it is a part of namespace
+          strcpy(name, dbase[i].name);
+          // CharToOem(name, name);
+          cout << name << dbase[i].birth_year << ' ' << dbase[i].pay << endl;
+          n_man++; mean_pay += dbase[i].pay;
+          not_found = false;
+        }
+    }
+    if(not_found) cout << "No person with this name." << endl;
+  }
+    if(n_man>0) cout << "Average pay: " << mean_pay / n_man << endl;
+    return 0;
 }
+
+//
+// Скорректировать программу: сделать так, чтобы "не мешали" пробельные символы перед фамилией
+// привести все в один регистр tolower(c), toupper(c)
+// адаптировать для русского языка и кодировок ASCII и ANSI (раскомментировать и попробовать)
+// функции для регистров русского языка tolower/toupper
+// вариант с библиотечными функциями ввода-вывода
