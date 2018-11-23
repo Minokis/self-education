1d84f6010cdf5c6f33e5d4921c4b5ac18eaf18dd
Anna Korniychuk <korniychuk.anna@gmail.com>
2018-11-23 11:33:03 +0300

    custom_cosh function with refs and default eps

--- labs/part_1_Structured/7_functions/7_1_standard_types/7_1_standard_types.cpp
+++ labs/part_1_Structured/7_functions/7_1_standard_types/7_1_standard_types.cpp
@@ -1,12 +1,17 @@
-// 7_1_standard_types.cpp: определяет точку входа для консольного приложения.
+// 7_1_standard_types.cpp: пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 // Write a program which outputs a table of values of function Ch(x) - hyperbolic cos
 // for x changing in input boundaries with input step. Taylor, precision is epsilon
 
-#include "stdafx.h"
+//#include "stdafx.h" // uncomment in Visual Studio
 #include <stdio.h>
 #include <math.h>
+#include <float.h> // for DBL_EPSILON
 
-double custom_cosh(double x, double eps, int &err);
+// Now we use const to show values that we won't change in the function
+// And also we use a default parameter
+double custom_cosh(const double x, int &err, const double eps=DBL_EPSILON);
+// reference variant is
+// double custom_cosh(const double &x, int &err, const double &eps);
 
 
 int main()
@@ -15,23 +20,24 @@ int main()
 	puts("Enter Xn, Xk, step, epsilon: ");
 	double Xn, Xk, dX, eps, y;
 	int err;
-	scanf_s("%lf%lf%lf%lf", &Xn, &Xk, &dX, &eps);
+	// scanf_s("%lf%lf%lf%lf", &Xn, &Xk, &dX, &eps); // uncomment in Visual Studio and comment the next line
+	scanf("%lf%lf%lf%lf", &Xn, &Xk, &dX, &eps);
 	// cntrl output
 	printf("\nYou entered %lf, %lf, %lf, %lf", Xn, Xk, dX, eps);
 	printf("---------------------\n");
 	printf("|      X      |         Y        |       math.h     |\n");
 	printf("---------------------\n");
 	for (double x = Xn; x <= Xk; x += dX) {
-		y = custom_cosh(x, eps, err);
+		y = custom_cosh(x, err);
 		if (err == 1) printf("|%9.2lf    |     Diverged!    |%14.6g    |\n", x, cosh(x));
 		else printf("|%9.2lf    |%14.6g    |%14.6g    |\n", x, y, cosh(x));
 	}
 	printf("---------------------\n");
-	
+
     return 0;
 }
 
-double custom_cosh(double x, double eps, int &err) {
+double custom_cosh(const double x, int &err, const double eps){
 	err = 0;
 	double current=1, y=current;
 	const int MaxIter = 500;
