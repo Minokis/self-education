f60d293498fb6f3cb5333291d030008f0e4ec488
Anna Korniychuk <korniychuk.anna@gmail.com>
2018-11-23 12:17:30 +0300

    Finish 7_2

---
+++ labs/part_1_Structured/7_functions/7_2_arrays_to_func.cpp
@@ -0,0 +1,46 @@
+// 2 arrays with n int numbers.
+// Which one has more positive elements?
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int n_posit(const int *arr, const int n);
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) { cout << "Usage: enter a filename with 2 arrays.\n"; return 1;}
+  ifstream fin(argv[1]);
+  if (!fin) { cout << "Couldn't open the file. :(\n"; return 1;}
+  int i, n;
+  fin >> n;
+  int *a = new int[n];
+  int *b = new int[n];
+  for (i = 0; i<n; i++) {
+    fin >> a[i];
+  }
+  for (i = 0; i<n; i++) {
+    fin >> b[i];
+  }
+  // cout << "\nControl output:\n";
+  // for (i = 0; i<n; i++) {
+  //   cout << a[i] << ' ';
+  // }
+  // cout << endl;
+  // for (i = 0; i<n; i++) {
+  //   cout << b[i] << ' ';
+  // }
+  // cout << endl;
+  n_posit(a, n) > n_posit(b, n) ? cout << "First array has more positive elements.\n" : \
+    n_posit(a, n) < n_posit(b, n) ? cout << "Second array has more positive elements.\n" : \
+    cout << "Arrays have the same number of positive elements.\n";
+
+  return 0;
+
+}
+
+int n_posit(const int *arr, const int n) {
+  int n_pos = 0;
+  for (int i = 0; i < n; i++) if(arr[i] > 0) n_pos++;
+  return n_pos;
+}
