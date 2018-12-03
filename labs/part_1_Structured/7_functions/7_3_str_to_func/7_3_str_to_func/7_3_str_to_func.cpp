e14e09304968ff97907cc2c6f91425c28ac1b8c8
Minokis <korniychuk.anna@gmail.com>
2018-12-03 17:25:18 +0300

    Finish 7_3

--- labs/part_1_Structured/7_functions/7_3_str_to_func/7_3_str_to_func/7_3_str_to_func.cpp
+++ labs/part_1_Structured/7_functions/7_3_str_to_func/7_3_str_to_func/7_3_str_to_func.cpp
@@ -10,13 +10,31 @@
 
 using namespace std;
 
-//int num_num()
+int num_num(const char *str);
 
 int main()
 {
 	ifstream fin("test.txt", ios::in);
 	if (!fin) { cout << "Could not open the file test.txt!\n"; return 1; }
+
+	const int len = 101;
+	char str[len];
+	int i = 1; 
+
+	while (fin.getline(str, len)) {
+		cout << "Line " << i << " has " << num_num(str) << " numbers." << endl;
+		i++;
+	}
 	
     return 0;
 }
 
+// Won't work if there's a  sentence with number at the end (with period)
+int num_num(const char *str) {
+	int count = 0;
+	while (*str) {
+		if (isdigit(*str) && !isdigit(*(str+1)) && *(str + 1) != '.') count++;
+		str++;
+	}
+	return count;
+}
